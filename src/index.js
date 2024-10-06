e6e23d11d6aaecfa2d31744eb3f7d681ffb8f5a1
Punith1117 <punith11172006@gmail.com>
2024-10-06 16:46:30 +0530

    remove commented code and unwanted functions of console-code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./styles.css";
 
 import { displayTasks } from "./display-tasks";
 import { displayProjects } from "./display-projects";
-import { handleProjectEdit } from "./project-edit";
 import { displayProjectTasks } from "./display-project-tasks";
 
 let tabs = document.querySelectorAll('.tab');
@@ -22,13 +21,6 @@ tabs.forEach((tab) => {
         }
     } )
 })
-// console.log('#press a/A to add a task');
-// console.log('#press d/D to delete a task');
-// console.log('#press e/E to edit a task');
-// console.log('#press p to add a task to a project');
-// console.log('#press shift and p to add a project');
-// console.log('#press shift and d to delete smtg in project');
-// console.log('#press shift and e to edit smtg in project');
 class Task {
     constructor(taskName, dueDate, priority, description) {
         this.taskName = taskName;
@@ -75,70 +67,6 @@ if (localStorage.getItem('projectsArray') == null) {
     console.log('new project auto-created');
 }
 
-
-
-// document.addEventListener('keydown', (event) => {
-//     if (event.shiftKey && event.key.toLowerCase() == 'p') {
-//         let projectName, dueDate, description;
-//         console.log('p and shift pressed');
-//         [projectName, dueDate, description] = inputProject();
-//         addProject(projectName, dueDate, description);
-//         return;
-//     } else if (event.key.toLowerCase() == 'p') {
-//         let projectNum;
-//         do {
-//             projectNum = prompt('Enter project num to add task:');
-//         } while ((isNaN(projectNum)) || (validProjectNum(parseInt(projectNum)) == false));
-//         let taskName, dueDate, description;
-//         [taskName, dueDate, description] = inputTask();
-//         addTaskToProject(projectNum, taskName, dueDate, description);
-//         return;
-//     } else if (event.shiftKey && event.key.toLowerCase() == 'd') {
-//         let validInputs = ['p', 't'];
-//         let userInput;
-//         do {
-//             userInput = prompt('What do you want to delete?-p-roject or a -t-ask?');
-//         } while (!validInputs.includes(userInput));
-//         if (userInput == 'p') {
-//             handleDeleteProject();
-//         } else {
-//             handleDeleteTask();
-//         }
-//         return;
-//     } else if (event.shiftKey && event.key.toLowerCase() == 'e') {
-//         let projectNum;
-//         do {
-//             projectNum = prompt('Enter the project number to edit:');
-//         } while ((isNaN(projectNum)) || (validProjectNum(parseInt(projectNum)) == false));
-//         handleProjectEdit(projectNum);
-//         displayProjects();
-//         return;
-//     }
-//     switch(event.key.toLowerCase()) {
-//         case 'a':
-//             let taskName, dueDate, description;
-//             [taskName, dueDate, description] = inputTask();
-//             addTask(taskName, dueDate, description);
-//             displayTasks();
-//             break;
-//         case 'd':
-//             let zeroTaskCheck = retrieveTasks();
-//             if (zeroTaskCheck.length == 0) {
-//                 console.log('nothing to delete');
-//                 return;
-//             }
-//             let delTaskNum;
-//             do {
-//                 delTaskNum = prompt('Enter task num to delete:');
-//             } while ((isNaN(delTaskNum)) || (validTaskNum(parseInt(delTaskNum)) == false));
-//             deleteTask(delTaskNum);
-//             break;
-//         case 'e':
-//             handleTaskEdit();
-//             break;
-//         }
-// })
-
 export function addTaskToProject(projectNum, taskName, dueDate, priority, description) {
     let pro = JSON.parse(localStorage.getItem('projectsArray'));//get projects from local storage
     let projectToAddTask = pro[projectNum];// 0-base indexed project num
@@ -173,64 +101,6 @@ export function deleteProject(projectNum) {//ProjectNum is 0-based index
     displayProjects();
 }
 
-// function handleTaskEdit() {//simple tasks related
-//     let zeroTaskCheck = retrieveTasks();
-//     if (zeroTaskCheck.length == 0) {
-//         console.log('nothing to edit');
-//         return;
-//     }
-//     let editTaskNum;
-//     do {
-//         editTaskNum = prompt('Enter task num to edit:');
-//     } while ((isNaN(editTaskNum)) || (validTaskNum(parseInt(editTaskNum)) == false));
-//     let key;
-//     let keys = ['n','N','du','DU','de','DE'];
-//     do {
-//         key = prompt('Enter which field to edit:-(name[n]|due date[du]|description[de])');
-//     } while (!keys.includes(key));
-//     let newTaskName;
-//     let taskToEdit;
-//     let ta = JSON.parse(localStorage.getItem('tasksArray'));
-//     switch(key) {
-//         case 'n':
-//         case 'N':
-//             do {
-//                 newTaskName = prompt('Enter Name:-');
-//             } while (newTaskName === null || newTaskName.trim() === '');
-//             taskToEdit = ta[editTaskNum-1];
-//             taskToEdit.taskName = newTaskName;
-//             ta[editTaskNum-1] = taskToEdit;
-//             writeTasksToStorage(ta);
-//             console.log('successfully edited');
-//             displayTasks();
-//             break;
-//         case 'du':
-//         case 'DU':
-//             let newDueDate;
-//             let dueDateRegex = /^((0|1|2)[0-9]|30|31)-(0[1-9]|1[0-2])-[2][0][0-9][0-9]$/
-//             do {
-//                 newDueDate = prompt('Enter date(DD-MM-YYY)');
-//             } while(dueDateRegex.test(newDueDate) == false);
-//             taskToEdit = ta[editTaskNum-1];
-//             taskToEdit.dueDate = newDueDate;
-//             ta[editTaskNum-1] = taskToEdit;
-//             writeTasksToStorage(ta);
-//             console.log('successfully edited');
-//             displayTasks();
-//             break;
-//         case 'de':
-//         case 'DE':
-//             let newDescription = prompt('description:-');
-//             taskToEdit = ta[editTaskNum-1];
-//             taskToEdit.description = newDescription;
-//             ta[editTaskNum-1] = taskToEdit;
-//             writeTasksToStorage(ta);
-//             console.log('successfully edited');
-//             displayTasks();
-//             break;
-//     }
-// }
-
 function writeTasksToStorage(ta) {
     localStorage.setItem('tasksArray', JSON.stringify(ta));
 }
@@ -253,65 +123,12 @@ export function formatDateDayFirst(date) {
     return `${day}-${month}-${year}`;
 }
 
-function inputTask() {
-    let taskName;
-    do {
-        taskName = prompt('Enter Name:-');
-    } while (taskName === null || taskName.trim() === '');
-    console.log(taskName);
-    let dueDate;
-    let dueDateRegex = /^((0|1|2)[0-9]|30|31)-(0[1-9]|1[0-2])-[2][0][0-9][0-9]$/
-    do {
-        dueDate = prompt('Enter date(DD-MM-YYY)');
-    } while(dueDateRegex.test(dueDate) == false)
-    console.log(dueDate);
-    let description = prompt('description:-');
-    console.log(description);
-    return [taskName, dueDate, description];
-}
-
-function inputProject() {
-    let projectName;
-    do {
-        projectName = prompt('Enter Project Name:-');
-    } while (projectName === null || projectName.trim() === '');
-    console.log(projectName);
-    let dueDate;
-    let dueDateRegex = /^((0|1|2)[0-9]|30|31)-(0[1-9]|1[0-2])-[2][0][0-9][0-9]$/
-    do {
-        dueDate = prompt('Enter date(DD-MM-YYY)');
-    } while(dueDateRegex.test(dueDate) == false)
-    console.log(dueDate);
-    let description = prompt('description:-');
-    console.log(description);
-    return [projectName, dueDate, description];
-}
-
 export function addProject(projectName, dueDate, priority, description) {
     let newProject = new Project(projectName, dueDate, priority, description);
     let pro = JSON.parse(localStorage.getItem('projectsArray'));
     pro.push(newProject);
     writeProjectsToStorage(pro);
     displayProjects();
-}
-
-function handleDeleteProject() {
-    let projectNum;
-    do {
-        projectNum = prompt('Enter project num to delete:');   
-    } while ((isNaN(projectNum)) || (validProjectNum(parseInt(projectNum)) == false));
-    let pro = JSON.parse(localStorage.getItem('projectsArray'))
-    let confirmDelete = prompt('Are you sure you want to delete ' + pro[projectNum-1].name + ' Enter y for yes');
-    if (confirmDelete == 'y') {
-        let removedProject = pro.splice((projectNum-1), 1);
-        console.log('Deleted Project:');
-        console.log(removedProject);
-        console.log('Updated Project list:');
-        writeProjectsToStorage(pro);
-        displayProjects();
-    } else {
-        console.log('delete aborted');
-    }
 }
 
 export function deleteProjectTask(projectIndex, taskIndex) {// project related, indexes are 0-based
@@ -323,40 +140,12 @@ export function deleteProjectTask(projectIndex, taskIndex) {// project related, 
     displayProjectTasks(projectIndex);
 }
 
-
-// function displayTasks() {
-//     tasks = retrieveTasks();
-//     console.log('Num of tasks: ' + tasks.length);
-//     console.log('Tasks:')
-//     tasks.forEach((task) => {
-//         console.log('Name: ' + task.taskName + ' __ ' + 'Due date: ' + task.dueDate + '__' + 'Description: ' + task.description);
-//     })
-// }
-
-// function displayProjects() {
-//     let projectsArray = JSON.parse(localStorage.getItem('projectsArray'));
-//     projectsArray.forEach(project => {
-//         console.log(project);
-//     })
-//     console.log('Num of projects: ' + projectsArray.length);
-// }
-
 function validTaskNum(num) {
     let ta = JSON.parse(localStorage.getItem('tasksArray'));//get tasks array from storage
     if ((num <= ta.length) && (num >= 1)) {
         return true;
     } else {
         console.log('invalid task num');
-        return false;
-    }
-}
-
-function validProjectNum(num) {
-    let proj = JSON.parse(localStorage.getItem('projectsArray'));//get projects array from storage
-    if ((num <= proj.length) && (num >= 1)) {
-        return true;
-    } else {
-        console.log('invalid project num');
         return false;
     }
 }
